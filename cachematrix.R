4620d1cdb4c0419a65d0d11b2573d9dadc5fe12c
AdrienAtallah <AdrienAtallah@gmail.com>
2015-12-26 14:48:04 -0800

    add explanatory comments and body from example in assignment description

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Adrien Atallah
 ## R Programming - Project 2
 
-## Write a short comment describing this function
+## makeCacheMatrix Creates a special "matrix" 
+## object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
+## casheSolve computes the inverse of the matrix returned by
+## makeCacheMatrix; if the inverse has already been calcuated
+## the cacheSolve retrieves the inverse from the cache.
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
 }
